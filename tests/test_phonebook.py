64d00c17d66a5d7c9a54e6c079053b12e4f54ac2
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-06 17:41:01 +0000

    Removed duplication with phonebook

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -9,20 +9,19 @@ class PhoneBookTest(unittest.TestCase):
         self.phonebook = Phonebook()
 
     def test_lookup_by_name(self):
-        phonebook.add("Bob", "12345")
-        number = phonebook.lookup("Bob")
+        self.phonebook.add("Bob", "12345")
+        number = self.phonebook.lookup("Bob")
         self.assertEqual("12345", number)
 
     def test_missing_name(self):
     # Checks to ensure lines within the with statement return an error and if does test will pass
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
 # Unit test skip so will skip test
     @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
-        phonebook = Phonebook()
     # Using assertTrue which means should return true for an empty phonebook
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
 
 

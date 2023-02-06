895cdbdcd8443266cc71ca2d022a4fbd505d60d2
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-06 19:03:36 +0000

    Changed method so unit tests pass

--- src/phonebook.py
+++ src/phonebook.py
@@ -11,4 +11,10 @@ class Phonebook:
         return self.numbers[name]
 
     def is_consistent(self):
+        for name1, number1 in self.numbers.items():
+            for name2, number2 in self.numbers.items():
+                if name1 == name2:
+                    continue
+                if number1.startswith(number2):
+                    return False
         return True

1be90c8a533719224894b24bc61b6f63791b083a
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-06 17:21:11 +0000

    Added another test this time with assertRaises and key error

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -9,3 +9,11 @@ class PhoneBookTest(unittest.TestCase):
         phonebook.add("Bob", "12345")
         number = phonebook.lookup("Bob")
         self.assertEqual("12345", number)
+
+    def test_missing_name(self):
+    # First contruct instance of unit under test which is Phonebook
+        phonebook = Phonebook()
+    # Checks to ensure lines within the with statement return an error and if does test will pass
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
+

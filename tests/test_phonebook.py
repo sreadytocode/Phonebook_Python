2c50d1967dd7cea5f184a3f4c7e592c3adcd4dd1
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-06 17:37:19 +0000

    Added setup and another test which is being skipped for now

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -4,16 +4,25 @@ from src.phonebook import Phonebook
 
 class PhoneBookTest(unittest.TestCase):
 
+    # First contruct instance of unit under test which is Phonebook in setup
+    def setUp(self):
+        self.phonebook = Phonebook()
+
     def test_lookup_by_name(self):
-        phonebook = Phonebook()
         phonebook.add("Bob", "12345")
         number = phonebook.lookup("Bob")
         self.assertEqual("12345", number)
 
     def test_missing_name(self):
-    # First contruct instance of unit under test which is Phonebook
-        phonebook = Phonebook()
     # Checks to ensure lines within the with statement return an error and if does test will pass
         with self.assertRaises(KeyError):
             phonebook.lookup("missing")
+
+# Unit test skip so will skip test
+    @unittest.skip("WIP")
+    def test_empty_phonebook_is_consistent(self):
+        phonebook = Phonebook()
+    # Using assertTrue which means should return true for an empty phonebook
+        self.assertTrue(phonebook.is_consistent())
+
 
